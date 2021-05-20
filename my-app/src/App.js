38ad06839c597c39f7547111777a83699940492f
tronnerbonner <59627722+tronnerbonner@users.noreply.github.com>
2021-05-20 12:10:47 -0600

    Added progress

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+//import FormValidation from './FormValidation'
+// import InputEvents from './events/InputEvents'
+// import MovementEvents from './events/MovementEvents'
+// import './events/Events.css'
+// import './Card.css'
+// import Card from './Card'
+import ShoppingList from './shoppingList/ShoppingList'
+import './shoppingList/ShoppingList.css'
+
+function App() {
+  return (
+    <div className="App">
+        <ShoppingList />
+    </div>
+  );
+}
+
+export default App;
